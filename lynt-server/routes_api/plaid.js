4e04f9d11ea5319f53d889fbdd737c4326b33ec3
twoducky <evanxandrews@gmail.com>
2017-01-05 16:18:32 -0800

    setup plaid authenticate

--- lynt-server/routes_api/plaid.js
+++ lynt-server/routes_api/plaid.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var plaid = require('plaid')
 var router = express.Router();
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
 var main = require('../config/main');
-
-var config = require('../config/main');
+var lynt = require('../config/plaid.json')
 var User = require('../models/user');
+
+var plaidClient = new plaid.Client(lynt.PLAID_CLIENT_ID,
+                                   lynt.PLAID_SECRET,
+                                   plaid.environments.tartan);
 
 // Bring in defined Passport Strategy
 require('../config/passport')(passport);
@@ -13,8 +17,7 @@ require('../config/passport')(passport);
 // Set up middleware
 var requireAuth = passport.authenticate('jwt', { session: false });
 
-router.post('/account', requireAuth, function(req, res) {
-  //[{"accuracy":30,"altitude":200,"heading":35,"latitude":250,"longitude":500,"speed":55,"timestamp":28172937}]
+router.post('/authenticate', requireAuth, function(req, res) {
 
   //verify JWT user
   jwt.verify(req.headers.authorization.replace('JWT ', ''), main['secret'], function(err, decoded) {
@@ -24,21 +27,24 @@ router.post('/account', requireAuth, function(req, res) {
       if (err)
         res.status(400).send(err);
 
-      var key = req.body.key;
+      var publicToken = req.body.public_token;
 
-      user.keys.push(key);
+      plaidClient.exchangeToken(publicToken, function(err, exchangeTokenRes){
+        if (err != null){
+          // Handle errors!
+        } else {
+          var accessToken = exchangeTokenRes.access_token;
+          plaidClient.getAuthUser(accessToken, function(err, authRes){
+            if (err != null){
+              // Handle errors!
+            } else {
+              var accounts = authRes.accounts;
 
-      console.log('saving user public key');
-
-      user.save(function(err) {
-
-        if (err){
-          console.log(err);
-          res.status(400).send(err);
+              // Return account data
+              res.json({accounts: accounts});
+            }
+          });
         }
-
-        console.log('saved');
-        res.status(201).json({ message: 'Bank Account Added! ~not really...' });
       });
     });
   });
